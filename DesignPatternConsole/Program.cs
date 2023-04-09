d2fbb648785d6e0d15d6831d70101622706f1d2e
Tying <542923663@qq.com>
2023-04-09 20:20:24 +0800

    Add Builder and Proxy pattern

--- DesignPatternConsole/Program.cs
+++ DesignPatternConsole/Program.cs
@@ -1,6 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BuilderPattern;
+using FactoryPattern;
+using FactoryPattern.SimpleFactory;
+using ProxyPattern;
 using SingletonPattern;
 
 Console.WriteLine(LazyIdGen.GetInstance().GetId());
 
+CacheConfigParserFactory simpleFactory = new CacheConfigParserFactory();
+ICacheConfigurationParser parser1 = simpleFactory.createParser("JSON");
+parser1.Load("1.json");
+Console.WriteLine(parser1.GetSection("ConnStr"));
+
+ResourcePoolConfig resourcePoolConfig = new ResourcePoolConfigBuilder()
+                                            .SetName("test")
+                                            .SetMaxTotal(10)
+                                            .SetMaxIdel(2)
+                                            .SetMinIdel(1)
+                                            .Build();
+Console.WriteLine(resourcePoolConfig);
+
+UserController userController = new UserControllerProxy(MetricUtils.getInstance());
+userController.login("tying", "123");
